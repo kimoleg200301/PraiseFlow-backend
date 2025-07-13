85410c691f52fd16cf21d6761716b76385dd7302
kim <o.kim@platonus.kz>
2025-07-14 02:35:26 +0500

    first commit

---
+++ src/main/java/com/kio/praiseflow/PraiseFlowApplication.java
@@ -0,0 +1,13 @@
+package com.kio.praiseflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraiseFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraiseFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kio/praiseflow/PraiseFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kio.praiseflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraiseFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
